6c262f26b8972331ea62ba1eb8c248d4b9d2ee20
Zeke <zeke.swepson@gmail.com>
2011-06-12 23:17:55 -0700

    Slow down spiders... again

--- src/com/ionmarkgames/ys/client/objects/enemies/Spider.java
+++ src/com/ionmarkgames/ys/client/objects/enemies/Spider.java
@@ -5,20 +5,6 @@ import com.ionmarkgames.ys.client.objects.Enemy;
 import com.ionmarkgames.ys.client.objects.You;
 
 public class Spider extends Enemy {
-
-	private enum Mode {
-		WAIT,
-		ATTACK,
-		RETURN
-	}
-	
-	private static final int MAX_THRESHOLD = 25;
-	
-	private Mode mode = Mode.WAIT;
-	
-	private int threshold = 0;
-	private int targetX = 0;
-	private int targetY = 0;
 	
 	private int homeX;
 	private int homeY;
@@ -29,38 +15,16 @@ public class Spider extends Enemy {
 		this.homeX = this.getX();
 		this.homeY = this.getY();
 		this.health = 1;
-		this.speed = 5;
+		this.speed = 2;
 		this.power = 1;
 	}
 	
 	@Override
 	public void act() {
-		this.threshold++;
-		switch(this.mode) {
-			case WAIT:
-				double part1 = Math.pow(( this.player.getCenterX() - this.getCenterX() ), 2);
-				double part2 = Math.pow(( this.player.getCenterY() - this.getCenterY() ), 2);
-				double underRadical = part1 + part2;
-				
-				if ((int)Math.sqrt(underRadical) / YSPanel.TILE_WIDTH < 4) {
-					this.targetX = player.getX();
-					this.targetY = player.getY();
-					this.threshold = 0;
-					this.mode = Mode.ATTACK;
-				}
-				break;
-			case ATTACK:
-				if (threshold > MAX_THRESHOLD || this.moveTowardsTarget(this.targetX, this.targetY)) {
-					this.mode = Mode.RETURN;
-					threshold = 0;
-				}
-				break;
-			case RETURN:
-				if (threshold > MAX_THRESHOLD || this.moveTowardsTarget(homeX, homeY)) {
-					this.mode = Mode.WAIT;
-					threshold = 0;
-				}
-				break;
+		if ((player.getX() == this.getX() || player.getY() == this.getY())) {
+			this.moveTowardsTarget(player.getX(), player.getY());
+		} else {
+			this.moveTowardsTarget(homeX, homeY);
 		}
 	}
 }
